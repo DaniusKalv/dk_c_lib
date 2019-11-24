af040ebf933baeda2516d2500e91bb01a343cd02
Danius Kalvaitis <danius.kalvaitis@gmail.com>
2019-11-24 08:30:28 +0000

    Created tlv320dac32 driver template.

---
+++ nordic/components/drivers_ext/tlv320dac32/tlv320dac32-internal.h
@@ -0,0 +1,10 @@
+/**
+ * @file        tlv320dac32-internal.c
+ * @author      Danius Kalvaitis (danius.kalvaitis@gmail.com)
+ * @brief       TLV320DAC32 codec driver internal defines.
+ * @version     0.1
+ * @date        2019-11-19
+ * 
+ * @copyright   Copyright (c) Danius Kalvaitis 2019 All rights reserved
+ * 
+ */
