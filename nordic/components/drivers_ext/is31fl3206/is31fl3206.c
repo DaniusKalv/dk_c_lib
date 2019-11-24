cc541e926033eb7765579dd7baf752a81ce84f0b
Danius Kalvaitis <danius.kalvaitis@gmail.com>
2019-11-24 08:30:04 +0000

    Created bare minimum is31fl3206 driver. Not all functionality implemented.

---
+++ nordic/components/drivers_ext/is31fl3206/is31fl3206.c
@@ -0,0 +1,175 @@
+/**
+ * @file        is31fl3206.c
+ * @author      Danius Kalvaitis (danius.kalvaitis@gmail.com)
+ * @brief       IS31FL3206 LED driver driver :)
+ * @version     0.1
+ * @date        2019-11-18
+ * 
+ * @copyright   Copyright (c) Danius Kalvaitis 2019 All rights reserved
+ * 
+ */
+
+#include "is31fl3206.h"
+#include "is31fl3206-internal.h"
+
+#define NRF_LOG_MODULE_NAME IS31FL3206
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+#define IS31FL3206_TWI_BUFFER_SIZE IS31FL3206_OUT_AMOUNT
+
+typedef struct 
+{
+	uint8_t reg_address;                        /**< Register address. */
+	uint8_t data[IS31FL3206_TWI_BUFFER_SIZE];   /**< Data buffer. */
+} is31fl3206_twi_write_t;
+
+static ret_code_t twi_write(nrfx_twi_t * p_i2c_instance, 
+                            uint8_t i2c_address,
+                            uint8_t reg,
+                            uint8_t * p_data,
+                            uint8_t data_length)
+{
+	is31fl3206_twi_write_t is31fl3206_write;
+
+	is31fl3206_write.reg_address = reg;
+	memcpy(is31fl3206_write.data, p_data, data_length);
+
+	data_length++; //Add the size of register byte
+
+	return nrfx_twi_tx(p_i2c_instance,
+	                   i2c_address,
+	                   (uint8_t *)&is31fl3206_write,
+	                   data_length,
+	                   false);
+}
+
+ret_code_t is31fl3206_init(is31fl3206_t * p_is31fl3206)
+{
+	NRF_LOG_INFO("Initialising IS31FL3206");
+
+	return is31fl3206_reset(p_is31fl3206);
+}
+
+ret_code_t is31fl3206_shutdown(is31fl3206_t * p_is31fl3206, bool shutdown)
+{
+	is31fl3206_shutdown_t shutdown_reg;
+	memset(&shutdown_reg, 0, sizeof(shutdown_reg));
+
+	shutdown_reg.normal_operation = !shutdown;
+
+	return twi_write(p_is31fl3206->p_i2c_instance, 
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_RESET,
+	                 (uint8_t *)&shutdown_reg,
+	                 sizeof(shutdown_reg));
+}
+
+ret_code_t is31fl3206_set_out_pwm(is31fl3206_t * p_is31fl3206,
+                                  is31fl3206_out_t out,
+                                  uint8_t pwm)
+{
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_PWM0 + out,
+	                 &pwm,
+	                 sizeof(pwm));
+}
+
+ret_code_t is31fl3206_update(is31fl3206_t * p_is31fl3206)
+{
+	uint8_t update_val = IS31FL3206_UPDATE_VAL;
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_UPDATE,
+	                 &update_val,
+	                 sizeof(update_val));
+}
+
+ret_code_t is31fl3206_set_out_current(is31fl3206_t * p_is31fl3206,
+	                                  is31fl3206_out_t out,
+	                                  is31fl3206_led_current_t led_current)
+{
+	is31fl3206_led_ctrl_t led_current_reg;
+	memset(&led_current_reg, 0, sizeof(led_current_reg));
+
+	led_current_reg.led_current = led_current;
+
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_LED_CURRENT_0 + out,
+	                 (uint8_t *)&led_current_reg,
+	                 sizeof(led_current_reg));
+}
+
+ret_code_t is31fl3206_shutdown_leds(is31fl3206_t * p_is31fl3206,
+                                    bool shutdown_leds)
+{
+	is31fl3206_global_ctrl_t global_ctrl;
+	memset(&global_ctrl, 0, sizeof(global_ctrl));
+
+	global_ctrl.shutdown_leds = shutdown_leds;
+
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_GLOBAL_CTRL,
+	                 (uint8_t *)&global_ctrl,
+	                 sizeof(global_ctrl));
+}
+
+ret_code_t is31fl3206_set_out_frequency(is31fl3206_t * p_is31fl3206,
+										is31fl3206_ofs_t output_frequency)
+{
+	is31fl3206_out_frequency_t output_frequency_reg;
+	memset(&output_frequency_reg, 0, sizeof(output_frequency_reg));
+
+	output_frequency_reg.ofs = output_frequency;
+	
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 p_is31fl3206->i2c_address,
+	                 IS31FL3206_OUT_FREQUENCY,
+	                 (uint8_t *)&output_frequency_reg,
+	                 sizeof(output_frequency_reg));
+}
+
+ret_code_t is31fl3206_reset(is31fl3206_t * p_is31fl3206)
+{
+	uint8_t reset_val = IS31FL3206_RESET_VAL;
+	bool detected_device = false;
+	uint8_t dummy = 0;
+
+	// for (uint8_t address = 1; address <= 127; address++)
+	// {
+	//     ret_code_t err_code = twi_write(p_is31fl3206->p_i2c_instance,
+	//                  address,
+	//                  IS31FL3206_RESET,
+	//                  &reset_val,
+	//                  sizeof(reset_val));
+	//     if (err_code == NRF_SUCCESS)
+	//     {
+	//         detected_device = true;
+	//         NRF_LOG_INFO("TWI device detected at address 0x%x.", address);
+	//     }
+	//     else
+	//     {
+	//         NRF_LOG_ERROR(NRF_LOG_ERROR_STRING_GET(err_code));
+	//     }
+	// }
+
+	// if (!detected_device)
+	// {
+	//     NRF_LOG_INFO("No device was found.");
+	// }
+
+	// twi_write(p_is31fl3206->p_i2c_instance,
+	// 				 0x6C,
+	// 				 IS31FL3206_RESET,
+	// 				 &reset_val,
+	// 				 sizeof(reset_val));
+
+	return twi_write(p_is31fl3206->p_i2c_instance,
+	                 0x6C,
+	                 IS31FL3206_RESET,
+	                 &reset_val,
+	                 sizeof(reset_val));
+}
---
+++ nordic/components/drivers_ext/is31fl3206/is31fl3206.h
@@ -0,0 +1,116 @@
+/**
+ * @file        is31fl3206.h
+ * @author      Danius Kalvaitis (danius.kalvaitis@gmail.com)
+ * @brief       IS31FL3206 LED driver driver :)
+ * @version     0.1
+ * @date        2019-11-18
+ * 
+ * @copyright   Copyright (c) Danius Kalvaitis 2019 All rights reserved
+ * 
+ */
+
+#ifndef IS31FL3206_H
+#define IS31FL3206_H
+
+#include "nrfx_twi.h"
+#include "sdk_errors.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct
+{
+	uint8_t _padding0       :7;
+	bool    normal_operation:1;
+} is31fl3206_shutdown_t;
+
+typedef enum
+{
+	IS31FL3206_LED_CURRENT_0        = 0x00,
+	IS31FL3206_LED_CURRENT_MAX      = 0x10,
+	IS31FL3206_LED_CURRENT_11_12    = 0x11,
+	IS31FL3206_LED_CURRENT_9_12     = 0x12,
+	IS31FL3206_LED_CURRENT_7_12     = 0x13,
+} is31fl3206_led_current_t;
+
+typedef struct
+{
+	is31fl3206_led_current_t led_current;
+} is31fl3206_led_ctrl_t;
+
+typedef struct
+{
+	uint8_t _padding0       :7;
+	bool    shutdown_leds   :1;
+} is31fl3206_global_ctrl_t;
+
+typedef enum
+{
+	IS31FL3206_OFS_24KHZ  = 0x00,
+	IS31FL3206_OFS_3_6KHZ = 0x01,
+} is31fl3206_ofs_t;
+
+typedef struct
+{
+	uint8_t             _padding0   :7;
+	is31fl3206_ofs_t    ofs         :1;
+} is31fl3206_out_frequency_t;
+
+typedef struct
+{
+	nrfx_twi_t    * p_i2c_instance; /**< Pointer to I2C instance. */
+	uint8_t         i2c_address;    /**< Device I2C slave address. */
+} is31fl3206_t;
+
+#define IS31FL3206_DEF(_name, _p_i2c_instance, _i2c_address)\
+static is31fl3206_t _name =                                 \
+{                                                           \
+	.p_i2c_instance = _p_i2c_instance,                      \
+	.i2c_address = _i2c_address                             \
+};
+
+typedef enum
+{
+	IS31FL3206_OUT1,
+	IS31FL3206_OUT2,
+	IS31FL3206_OUT3,
+	IS31FL3206_OUT4,
+	IS31FL3206_OUT5,
+	IS31FL3206_OUT6,
+	IS31FL3206_OUT7,
+	IS31FL3206_OUT8,
+	IS31FL3206_OUT9,
+	IS31FL3206_OUT10,
+	IS31FL3206_OUT11,
+	IS31FL3206_OUT12,
+	IS31FL3206_OUT_AMOUNT,
+} is31fl3206_out_t;
+
+ret_code_t is31fl3206_init(is31fl3206_t * p_is31fl3206);
+
+ret_code_t is31fl3206_shutdown(is31fl3206_t * p_is31fl3206, bool shutdown);
+
+ret_code_t is31fl3206_set_out_pwm(is31fl3206_t * p_is31fl3206,
+                                  is31fl3206_out_t out,
+                                  uint8_t pwm);
+
+ret_code_t is31fl3206_update(is31fl3206_t * p_is31fl3206);
+
+ret_code_t is31fl3206_set_out_current(is31fl3206_t * p_is31fl3206,
+                                      is31fl3206_out_t out,
+                                      is31fl3206_led_current_t led_current);
+
+ret_code_t is31fl3206_shutdown_leds(is31fl3206_t * p_is31fl3206,
+                                    bool shutdown_leds);
+
+ret_code_t is31fl3206_set_out_frequency(is31fl3206_t * p_is31fl3206,
+                                        is31fl3206_ofs_t output_frequency);
+
+ret_code_t is31fl3206_reset(is31fl3206_t * p_is31fl3206);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //IS31FL3206_H
