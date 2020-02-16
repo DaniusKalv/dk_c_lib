9376c207a0624258f277baba5c4c5f30f5a81cdd
Danius Kalvaitis <danius.kalvaitis@gmail.com>
2020-02-16 17:17:10 +0000

    Created phil it up service

--- common/components/ble/dk_ble_uuids/dk_ble_uuids.h
+++ common/components/ble/dk_ble_uuids/dk_ble_uuids.h
@@ -15,25 +15,30 @@
 #define DK_BLE_UUID_BASE    {{0x8F, 0x02, 0xED, 0x3E, 0x20, 0xE9, 0x4F, 0x60, 0xB1, 0x7A, 0xA4, 0x78, 0x1B, 0x33, 0x43, 0x01}} // 128-bit base UUID
 
 // Accelerometer service
-#define DK_BLE_UUID_ACC_SERVICE                     0x4B24  /**< Accelerometer service UUID. */
-#define DK_BLE_UUID_ACC_RAW_CHARACTERISTIC          0x4B25  /**< Accelerometer raw data characteristic UUID. */
-#define DK_BLE_UUID_ACC_ALERT_CHARACTERISTIC        0x4B26  /**< Accelerometer alert characteristic UUID. */
-#define DK_BLE_UUID_ACC_CONFIG_CHARACTERISTIC       0x4B27  /**< Accelerometer configuration characteristic UUID. */
+#define DK_BLE_UUID_ACC_SERVICE                         0x4B24  /**< Accelerometer service UUID. */
+#define DK_BLE_UUID_ACC_RAW_CHARACTERISTIC              0x4B25  /**< Accelerometer raw data characteristic UUID. */
+#define DK_BLE_UUID_ACC_ALERT_CHARACTERISTIC            0x4B26  /**< Accelerometer alert characteristic UUID. */
+#define DK_BLE_UUID_ACC_CONFIG_CHARACTERISTIC           0x4B27  /**< Accelerometer configuration characteristic UUID. */
 
 // Gyro service
-#define DK_BLE_UUID_GYRO_SERVICE                    0x3044  /**< Gyro service UUID. */
-#define DK_BLE_UUID_GYRO_RAW_CHARACTERISTIC         0x3045  /**< Gyro raw data characteristic UUID. */
-#define DK_BLE_UUID_GYRO_ALERT_CHARACTERISTIC       0x3046  /**< Gyro alert characteristic UUID. */
-#define DK_BLE_UUID_GYRO_CONFIG_CHARACTERISTIC      0x3047  /**< Gyro configuration characteristic UUID. */
+#define DK_BLE_UUID_GYRO_SERVICE                        0x3044  /**< Gyro service UUID. */
+#define DK_BLE_UUID_GYRO_RAW_CHARACTERISTIC             0x3045  /**< Gyro raw data characteristic UUID. */
+#define DK_BLE_UUID_GYRO_ALERT_CHARACTERISTIC           0x3046  /**< Gyro alert characteristic UUID. */
+#define DK_BLE_UUID_GYRO_CONFIG_CHARACTERISTIC          0x3047  /**< Gyro configuration characteristic UUID. */
 
 // Magnetometer service
-#define DK_BLE_UUID_MAG_SERVICE                     0xCB26  /**< Mag service UUID. */
-#define DK_BLE_UUID_MAG_RAW_CHARACTERISTIC          0xCB27  /**< Mag raw data characteristic UUID. */
-#define DK_BLE_UUID_MAG_ALERT_CHARACTERISTIC        0xCB28  /**< Mag alert characteristic UUID. */
-#define DK_BLE_UUID_MAG_CONFIG_CHARACTERISTIC       0xCB29  /**< Mag configuration characteristic UUID. */
+#define DK_BLE_UUID_MAG_SERVICE                         0xCB26  /**< Mag service UUID. */
+#define DK_BLE_UUID_MAG_RAW_CHARACTERISTIC              0xCB27  /**< Mag raw data characteristic UUID. */
+#define DK_BLE_UUID_MAG_ALERT_CHARACTERISTIC            0xCB28  /**< Mag alert characteristic UUID. */
+#define DK_BLE_UUID_MAG_CONFIG_CHARACTERISTIC           0xCB29  /**< Mag configuration characteristic UUID. */
 
 // Mr Pickle service
-#define DK_BLE_UUID_MR_PICKLE_SERVICE               0x8746  /**< Mr Pickle service UUID. */
-#define DK_BLE_UUID_MR_PICKLE_MODE_CHARACTERISTIC   0x8747  /**< Mr Pickle mode characterisitic UUID. */
+#define DK_BLE_UUID_MR_PICKLE_SERVICE                   0x8746  /**< Mr Pickle service UUID. */
+#define DK_BLE_UUID_MR_PICKLE_MODE_CHARACTERISTIC       0x8747  /**< Mr Pickle mode characterisitic UUID. */
+
+#define DK_BLE_UUID_PHIL_IT_UP_SERVICE                  0xFD98  /**< Phil It Up service UUID. */
+#define DK_BLE_UUID_PHIL_IT_UP_AMB_TMP_CHARACTERISTIC   0xFD99  /**< Phil It Up ambient temperature characteristic UUID. */
+#define DK_BLE_UUID_PHIL_IT_UP_MUG_TMP_CHARACTERISTIC   0xFD9A  /**< Phil It Up mug temperature characteristic UUID. */
+#define DK_BLE_UUID_PHIL_IT_UP_MUG_UP_CHARACTERISTIC    0xFD9B  /**< Phil It Up mug up characteristic UUID. */
 
 #endif // DK_BLE_UUIDS_H
---
+++ nordic/components/ble/dk_ble_services/dk_ble_phil_it_up/dk_ble_phil_it_up.c
@@ -0,0 +1,278 @@
+/**
+ * @file        dk_ble_phil_it_up.c
+ * @author      Danius Kalvaitis (danius.kalvaitis@gmail.com)
+ * @brief       Phil It Up service.
+ * @details     Supports multiple peripheral links.
+ * @version     0.1
+ * @date        2020-02-16
+ * 
+ * @copyright   Copyright (c) Danius Kalvaitis 2020 All rights reserved
+ * 
+ */
+
+#include "dk_ble_phil_it_up.h"
+#include "app_error.h"
+
+#define NRF_LOG_MODULE_NAME BLE_PHIL_IT_UP
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+/**
+ * @brief       Function for handling the @ref BLE_GATTS_EVT_WRITE event from the SoftDevice.
+ * 
+ * @param[in]   p_dk_ble_phil_it_up     Pointer to Phil it Up service instance.
+ * @param[in]   p_ble_evt               Pointer to the event received from BLE stack.
+ */
+static void on_write(dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, ble_evt_t const * p_ble_evt)
+{
+	ble_gatts_evt_write_t const * p_evt_write = &p_ble_evt->evt.gatts_evt.params.write;
+	dk_ble_phil_it_up_evt_t dk_ble_phil_it_up_evt;
+	dk_ble_phil_it_up_evt.conn_handle = p_ble_evt->evt.gatts_evt.conn_handle;
+
+	if(p_evt_write->handle == p_dk_ble_phil_it_up->char_handles.amb.cccd_handle)
+	{
+		if(ble_srv_is_notification_enabled(p_evt_write->data))
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_AMB_NOTIFICATIONS_ENABLED;
+		}
+		else
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_AMB_NOTIFICATIONS_DISABLED;
+		}
+		p_dk_ble_phil_it_up->evt_handler(&dk_ble_phil_it_up_evt);
+	}
+	else if(p_evt_write->handle == p_dk_ble_phil_it_up->char_handles.obj.cccd_handle)
+	{
+		if(ble_srv_is_notification_enabled(p_evt_write->data))
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_OBJ_NOTIFICATIONS_ENABLED;
+		}
+		else
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_OBJ_NOTIFICATIONS_DISABLED;
+		}
+		p_dk_ble_phil_it_up->evt_handler(&dk_ble_phil_it_up_evt);
+	}
+	else if(p_evt_write->handle == p_dk_ble_phil_it_up->char_handles.mug_up.cccd_handle)
+	{
+		if(ble_srv_is_notification_enabled(p_evt_write->data))
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_MUG_UP_NOTIFICATIONS_ENABLED;
+		}
+		else
+		{
+			dk_ble_phil_it_up_evt.evt_type = DK_BLE_PHIL_IT_UP_EVT_MUG_UP_NOTIFICATIONS_DISABLED;
+		}
+		p_dk_ble_phil_it_up->evt_handler(&dk_ble_phil_it_up_evt);
+	}
+}
+
+/**
+ * @brief       Add ambient temperature characteristic.
+ * 
+ * @param[in]   p_dk_ble_phil_it_up    Pointer to Phil it Up service instance.
+ * 
+ * @retval      NRF_SUCCESS If the characteristic was successfully added. Otherwise, an error code from characteristic_add is returned.
+ */
+static uint32_t dk_ble_phil_it_up_amb_tmp_characteristic_add(dk_ble_phil_it_up_t * p_dk_ble_phil_it_up)
+{
+	uint8_t initial_value[DK_BLE_PHIL_IT_UP_AMB_CHAR_SIZE] = { 0 };
+	ble_gatts_char_pf_t temperature_amb_pf;
+	memset(&temperature_amb_pf, 0, sizeof(temperature_amb_pf));
+	
+	temperature_amb_pf.format = BLE_GATT_CPF_FORMAT_FLOAT32;
+	temperature_amb_pf.unit = ORG_BLUETOOTH_UNIT_THERMODYNAMIC_TEMPERATURE_DEGREE_CELCIUS;	
+
+	ble_add_char_params_t temperature_amb_char_params;
+	memset(&temperature_amb_char_params, 0, sizeof(temperature_amb_char_params));
+
+	temperature_amb_char_params.uuid = DK_BLE_UUID_PHIL_IT_UP_AMB_TMP_CHARACTERISTIC;
+	temperature_amb_char_params.uuid_type = p_dk_ble_phil_it_up->uuid_type;
+	temperature_amb_char_params.max_len = DK_BLE_PHIL_IT_UP_AMB_CHAR_SIZE;
+	temperature_amb_char_params.init_len = DK_BLE_PHIL_IT_UP_AMB_CHAR_SIZE;
+	temperature_amb_char_params.p_init_value = initial_value;
+	temperature_amb_char_params.char_props.read = 1;
+	temperature_amb_char_params.char_props.notify = 1;
+	temperature_amb_char_params.read_access = SEC_OPEN;
+	temperature_amb_char_params.cccd_write_access = SEC_OPEN;
+	temperature_amb_char_params.p_user_descr = NULL;
+	temperature_amb_char_params.p_presentation_format = &temperature_amb_pf;
+
+	return characteristic_add(p_dk_ble_phil_it_up->service_handle, &temperature_amb_char_params, &p_dk_ble_phil_it_up->char_handles.amb);
+}
+
+/**
+ * @brief       Add object (mug) temperature characteristic.
+ * 
+ * @param[in]   p_dk_ble_phil_it_up    Pointer to Phil it Up service instance.
+ * 
+ * @retval      NRF_SUCCESS If the characteristic was successfully added. Otherwise, an error code from characteristic_add is returned.
+ */
+static uint32_t dk_ble_phil_it_up_mug_tmp_characteristic_add(dk_ble_phil_it_up_t * p_dk_ble_phil_it_up)
+{
+	uint8_t initial_value[DK_BLE_PHIL_IT_UP_OBJ_CHAR_SIZE] = { 0 };
+	ble_gatts_char_pf_t temperature_obj_pf;
+	memset(&temperature_obj_pf, 0, sizeof(temperature_obj_pf));
+	
+	temperature_obj_pf.format = BLE_GATT_CPF_FORMAT_FLOAT32;
+	temperature_obj_pf.unit = ORG_BLUETOOTH_UNIT_THERMODYNAMIC_TEMPERATURE_DEGREE_CELCIUS;	
+
+	ble_add_char_params_t temperature_obj_char_params;
+	memset(&temperature_obj_char_params, 0, sizeof(temperature_obj_char_params));
+
+	temperature_obj_char_params.uuid = DK_BLE_UUID_PHIL_IT_UP_MUG_TMP_CHARACTERISTIC;
+	temperature_obj_char_params.uuid_type = p_dk_ble_phil_it_up->uuid_type;
+	temperature_obj_char_params.max_len = DK_BLE_PHIL_IT_UP_OBJ_CHAR_SIZE;
+	temperature_obj_char_params.init_len = DK_BLE_PHIL_IT_UP_OBJ_CHAR_SIZE;
+	temperature_obj_char_params.p_init_value = initial_value;
+	temperature_obj_char_params.char_props.read = 1;
+	temperature_obj_char_params.char_props.notify = 1;
+	temperature_obj_char_params.read_access = SEC_OPEN;
+	temperature_obj_char_params.cccd_write_access = SEC_OPEN;
+	temperature_obj_char_params.p_user_descr = NULL;
+	temperature_obj_char_params.p_presentation_format = &temperature_obj_pf;
+
+	return characteristic_add(p_dk_ble_phil_it_up->service_handle, &temperature_obj_char_params, &p_dk_ble_phil_it_up->char_handles.obj);
+}
+
+/**
+ * @brief       Add mug up characteristic to Phil it Up service.
+ * 
+ * @param[in]   p_dk_ble_phil_it_up    Pointer to Phil it Up service instance.
+ * @param[in]   initial_value           Initial mug up value.
+ * 
+ * @retval      NRF_SUCCESS If the characteristic was successfully added. Otherwise, an error code from characteristic_add is returned.
+ */
+static uint32_t dk_ble_phil_it_up_mug_up_characteristic_add(dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, uint8_t initial_value)
+{
+	ble_gatts_char_pf_t temperature_force_pf;
+	memset(&temperature_force_pf, 0, sizeof(temperature_force_pf));
+	
+	temperature_force_pf.format = BLE_GATT_CPF_FORMAT_UINT8;
+
+	ble_add_char_params_t temperature_force_char_params;
+	memset(&temperature_force_char_params, 0, sizeof(temperature_force_char_params));
+
+	temperature_force_char_params.uuid = DK_BLE_UUID_PHIL_IT_UP_MUG_UP_CHARACTERISTIC;
+	temperature_force_char_params.uuid_type = p_dk_ble_phil_it_up->uuid_type;
+	temperature_force_char_params.max_len = DK_BLE_PHIL_IT_UP_MUG_UP_CHAR_SIZE;
+	temperature_force_char_params.init_len = DK_BLE_PHIL_IT_UP_MUG_UP_CHAR_SIZE;
+	temperature_force_char_params.p_init_value = &initial_value;
+	temperature_force_char_params.char_props.read = 1;
+	temperature_force_char_params.char_props.notify = 1;
+	temperature_force_char_params.read_access = SEC_OPEN;
+	temperature_force_char_params.cccd_write_access = SEC_OPEN;
+	temperature_force_char_params.p_user_descr = NULL;
+	temperature_force_char_params.p_presentation_format = &temperature_force_pf;
+
+	return characteristic_add(p_dk_ble_phil_it_up->service_handle, &temperature_force_char_params, &p_dk_ble_phil_it_up->char_handles.mug_up);
+}
+
+void dk_ble_phil_it_up_on_ble_evt(ble_evt_t const * p_ble_evt, void * p_context)
+{
+	if((p_context == NULL) || (p_ble_evt == NULL)) return;
+
+	dk_ble_phil_it_up_t * p_dk_ble_phil_it_up = (dk_ble_phil_it_up_t *)p_context;
+	
+	switch (p_ble_evt->header.evt_id)
+	{
+		case BLE_GATTS_EVT_WRITE:
+			on_write(p_dk_ble_phil_it_up, p_ble_evt);
+			break;
+		default:
+			break;
+	}
+}
+
+ret_code_t dk_ble_phil_it_up_init(dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, dk_ble_phil_it_up_config_t * p_phil_it_up_config)
+{
+	VERIFY_PARAM_NOT_NULL(p_dk_ble_phil_it_up);
+	VERIFY_PARAM_NOT_NULL(p_phil_it_up_config);
+	VERIFY_PARAM_NOT_NULL(p_phil_it_up_config->evt_handler);
+
+	NRF_LOG_INFO("Initializing BLE Phil It Up service.");
+
+	ret_code_t		err_code;
+	ble_uuid_t		ble_uuid;
+	ble_uuid128_t	dk_ble_phil_it_up_base_uuid = DK_BLE_UUID_BASE;
+
+	p_dk_ble_phil_it_up->evt_handler = p_phil_it_up_config->evt_handler;
+
+	err_code = sd_ble_uuid_vs_add(&dk_ble_phil_it_up_base_uuid, &p_dk_ble_phil_it_up->uuid_type);
+	VERIFY_SUCCESS(err_code);
+
+	ble_uuid.type = p_dk_ble_phil_it_up->uuid_type;
+	ble_uuid.uuid = DK_BLE_UUID_PHIL_IT_UP_SERVICE;
+
+	err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &ble_uuid, &p_dk_ble_phil_it_up->service_handle);
+	VERIFY_SUCCESS(err_code);
+	
+	err_code = dk_ble_phil_it_up_amb_tmp_characteristic_add(p_dk_ble_phil_it_up);
+	VERIFY_SUCCESS(err_code);
+
+	err_code = dk_ble_phil_it_up_mug_tmp_characteristic_add(p_dk_ble_phil_it_up);
+	VERIFY_SUCCESS(err_code);
+
+	err_code = dk_ble_phil_it_up_mug_up_characteristic_add(p_dk_ble_phil_it_up, (uint8_t) p_phil_it_up_config->initial_mug_up_val);
+	VERIFY_SUCCESS(err_code);
+
+	return NRF_SUCCESS;
+}
+
+ret_code_t dk_ble_phil_it_up_amb_tmp_notify(uint16_t conn_handle, dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, float temperature)
+{
+	ble_gatts_hvx_params_t hvx_params;
+	uint16_t length = sizeof(temperature);
+
+	memset(&hvx_params, 0, sizeof(hvx_params));
+
+	hvx_params.handle = p_dk_ble_phil_it_up->char_handles.amb.value_handle;
+	hvx_params.type = BLE_GATT_HVX_NOTIFICATION;
+	hvx_params.p_len = &length;
+	hvx_params.p_data = (uint8_t *)&temperature;
+
+	return sd_ble_gatts_hvx(conn_handle, &hvx_params);
+}
+
+ret_code_t dk_ble_phil_it_up_mug_tmp_notify(uint16_t conn_handle, dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, float temperature)
+{
+	ble_gatts_hvx_params_t hvx_params;
+	uint16_t length = sizeof(temperature);
+
+	memset(&hvx_params, 0, sizeof(hvx_params));
+
+	hvx_params.handle = p_dk_ble_phil_it_up->char_handles.obj.value_handle;
+	hvx_params.type = BLE_GATT_HVX_NOTIFICATION;
+	hvx_params.p_len = &length;
+	hvx_params.p_data = (uint8_t *)&temperature;
+
+	return sd_ble_gatts_hvx(conn_handle, &hvx_params);
+}
+
+ret_code_t dk_ble_phil_it_up_mug_up_notify(uint16_t conn_handle, dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, bool mug_up)
+{
+	ble_gatts_hvx_params_t hvx_params;
+	uint16_t length = DK_BLE_PHIL_IT_UP_MUG_UP_CHAR_SIZE;
+	uint8_t mug_up_val = (uint8_t)mug_up;
+
+	memset(&hvx_params, 0, sizeof(hvx_params));
+
+	hvx_params.handle = p_dk_ble_phil_it_up->char_handles.mug_up.value_handle;
+	hvx_params.type = BLE_GATT_HVX_NOTIFICATION;
+	hvx_params.p_len = &length;
+	hvx_params.p_data = &mug_up_val;
+
+	return sd_ble_gatts_hvx(conn_handle, &hvx_params);
+}
+
+ret_code_t dk_ble_phil_it_up_mug_up_value_set(uint16_t conn_handle, dk_ble_phil_it_up_t * p_dk_ble_phil_it_up, bool mug_up)
+{
+	ble_gatts_value_t ble_gatts_value;
+	uint8_t mug_up_val = (uint8_t)mug_up;
+	
+	ble_gatts_value.len = DK_BLE_PHIL_IT_UP_MUG_UP_CHAR_SIZE;
+	ble_gatts_value.offset = 0;
+	ble_gatts_value.p_value = &mug_up_val;
+
+	return sd_ble_gatts_value_set(conn_handle, p_dk_ble_phil_it_up->char_handles.mug_up.value_handle, &ble_gatts_value);
+}
